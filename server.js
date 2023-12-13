65652588c4b6b5b3f2664d9ccf44c75da78da9cb
Shreyansh <shreyansh@codalent.com>
2023-12-13 21:34:24 +0530

    Header chages

--- server.js
+++ server.js
@@ -7,40 +7,40 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-router.get("/", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "index.ejs"));
+router.get('/', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'index.ejs'), { path: '/' });
 });
 
-router.get("/service", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "service.ejs"));
+router.get('/service', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'service.ejs'), { path: '/service' });
 });
 
-router.get("/about", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "about.ejs"));
+router.get('/about', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'about.ejs'), { path: '/about' });
 });
 
-router.get("/portfolio", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "portfolio.ejs"));
+router.get('/portfolio', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'portfolio.ejs'), { path: '/portfolio' });
 });
 
-router.get("/team", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "team.ejs"));
+router.get('/team', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'team.ejs'), { path: '/team' });
 });
 
-router.get("/pricing", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "pricing.ejs"));
+router.get('/pricing', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'pricing.ejs'), { path: '/pricing' });
 });
 
-router.get("/contact", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "contact.ejs"));
+router.get('/contact', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'contact.ejs'), { path: '/contact' });
 });
 
-router.get("/blog", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "blog.ejs"));
+router.get('/blog', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'blog.ejs'), { path: '/blog' });
 });
 
-router.get("/single-post", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "single-post.ejs"));
+router.get('/single-post', async (req, res) => {
+    res.render(path.join(__dirname, 'views', 'ejs', 'single-post.ejs'), { path: 'single-post' });
 });
 
 router.get("*", async (req, res) => {
