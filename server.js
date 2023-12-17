20d1f24ec19e9cef7632b199ca9f63060825ffdc
Shreyansh <shreyansh@codalent.com>
2023-12-17 16:42:11 +0530

    Console

--- server.js
+++ server.js
@@ -8,47 +8,49 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 router.get('/', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'index.ejs'), { path: '/' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'index.ejs'), { path: '/' });
 });
 
+console.log("first")
+
 router.get('/service', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'service.ejs'), { path: '/service' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'service.ejs'), { path: '/service' });
 });
 
 router.get('/about', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'about.ejs'), { path: '/about' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'about.ejs'), { path: '/about' });
 });
 
 router.get('/portfolio', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'portfolio.ejs'), { path: '/portfolio' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'portfolio.ejs'), { path: '/portfolio' });
 });
 
 router.get('/team', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'team.ejs'), { path: '/team' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'team.ejs'), { path: '/team' });
 });
 
 router.get('/pricing', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'pricing.ejs'), { path: '/pricing' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'pricing.ejs'), { path: '/pricing' });
 });
 
 router.get('/contact', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'contact.ejs'), { path: '/contact' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'contact.ejs'), { path: '/contact' });
 });
 
 router.get('/blog', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'blog.ejs'), { path: '/blog' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'blog.ejs'), { path: '/blog' });
 });
 
 router.get('/single-post', async (req, res) => {
-    res.render(path.join(__dirname, 'views', 'ejs', 'single-post.ejs'), { path: 'single-post' });
+  res.render(path.join(__dirname, 'views', 'ejs', 'single-post.ejs'), { path: 'single-post' });
 });
 
 router.get("*", async (req, res) => {
-    res.render(path.join(__dirname, "views", "ejs", "404.ejs"));
+  res.render(path.join(__dirname, "views", "ejs", "404.ejs"));
 });
 
 app.use(router);
 
 app.listen(port, () => {
-    console.log("Server started at port " + port);
+  console.log("Server started at port " + port);
 });
